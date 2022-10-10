1137974b357579bfff448d521e15b0de55d991f0
Dmitriy <dfeeler@yandex.ru>
2022-10-10 16:03:56 -0400

    add dir Example

---
+++ Example/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Example
+{
+    class Program
+    {
+        static string[] GetUserData()
+        {
+            string[] result = new string[] { };
+
+            return result;
+        }
+
+        static string[] ReadUserDataFromFile()
+        {
+            string[] result = new string[] { };
+            
+            return result;
+        }
+
+        static string[] SetUserDataDefault()
+        {
+            string[] result = new string[]{"Russia", "Denmark", "Kazan"};
+            
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
